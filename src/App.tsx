a0ff90473f9aec451fe4d0e656c164b726221364
dangtrunghieu <dangtrunghieu.dthvhc@gmail.com>
2026-03-04 23:01:22 +0700

    feat: add Vercel Analytics for visitor tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -40,6 +41,7 @@ export default function App() {
         </Routes>
       </main>
       <Footer />
+      <Analytics />
     </BrowserRouter>
   );
 }
